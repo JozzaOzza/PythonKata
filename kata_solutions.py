69c03ae68fb3c05de9c6b79177227fa52000d790
JozzaOzza <JamieOrr13@gmail.com>
2023-03-26 11:17:48 +0100

    first kata solution worked, renamed solutions file

--- kata_solutions.py
+++
@@ -1,4 +0,0 @@
-class KataSolutions():
-    
-    def simple_addition(x, y):
-        return x + y
---
+++ kata_solutions_1.py
@@ -0,0 +1,20 @@
+class KataSolutions():
+    
+    def simple_addition(x, y):
+        return x + y
+
+    def cakes(recipe, ingredients): # answer is equal to the ratio of the ingredients element with the smallest ratio 
+                                    # to the equivalent recipe element (unless the ratio < 1 or it doesn't exist)
+        pre_loop = True
+        min_quantity = 0
+        
+        for k, v in recipe.items():
+            if not k in ingredients or v > ingredients[k]:
+               return 0
+            ratio = ingredients[k] // v
+            # if ratio of ingredient in ingredients : recipe is less than min_quanity, reassign min_quantity
+            min_quantity = ratio if pre_loop else ratio if ratio < min_quantity else min_quantity
+            pre_loop = False
+            
+        return min_quantity    
+           
